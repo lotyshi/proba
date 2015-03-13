a55a6d84ddb8eab3df4c3a67628683ffbca757fb
Iaroslav Lotysh <I.Lotysh@tt.com.pl>
2015-03-13 11:18:59 +0100

    TEST-1 test4 11:18

--- proba/src/main/java/lotyshi_proba/proba/App.java
+++ proba/src/main/java/lotyshi_proba/proba/App.java
@@ -11,6 +11,5 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World 1!" );
         System.out.println( "Hello World 1!" );
-        System.out.println( "Hello World 1!" );
     }
 }
